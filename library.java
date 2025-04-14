e590ffdd5b35793c72a9284ca4514af1c5f9878c
Vmachine23 <wazhavati@gmail.com>
2025-04-14 16:19:08 +0200

    Create library.java
    This is just a draft yall can do some changes this is for the first class
    we have 3 fields for book titles , cerial numbers and book author

---
+++ library.java
@@ -0,0 +1,37 @@
+public class library {
+String title;
+  int cerial ;
+  String author ;
+
+  public library library (String title, int cerial , String author ){
+this.title=title;
+this.cerial = cerial;
+this.author = author; 
+  }
+public String getTitle(){
+return title;
+}
+public int getCerial(){
+return cerial;
+}
+public String getAuthor(){
+return author;
+}
+
+}
+}
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
