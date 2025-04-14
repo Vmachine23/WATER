233c32cbc21ab8dd5bfdc4bfd5a7d8c231edc9c9
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-14 16:18:49 +0100

    Update library.java
    Restructured getters and setters.

--- library.java
+++ library.java
@@ -1,24 +1,22 @@
 public class library {
-String title;
-  int cerial ;
-  String author ;
+    String title;
+    int cerial;
+    String author;
 
-  public library library (String title, int cerial , String author ){
-this.title=title;
-this.cerial = cerial;
-this.author = author; 
+  public library(String title, int cerial , String author){
+    this.title = title;
+    this.cerial = cerial;
+    this.author = author; 
   }
-public String getTitle(){
-return title;
-}
-public int getCerial(){
-return cerial;
-}
-public String getAuthor(){
-return author;
-}
-
-}
+  public String getTitle(){
+    return title;
+  }
+  public int getCerial(){
+    return cerial;
+  }
+  public String getAuthor(){
+    return author;
+  }
 }
 
 
