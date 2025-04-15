fd218602b445b57b66fc4a410a47b94f91d3dff4
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-15 17:00:07 +0200

    Update library.java
    Added setters for three fields. Changed cerial to serialnumber.

--- library.java
+++ library.java
@@ -1,35 +1,29 @@
 public class library {
     String title;
-    int cerialnumber;
+    int serialnumber;
     String author;
 
-  public library(String title, int cerial , String author){
+  public library(String title, int serialnumber , String author){
     this.title = title;
-    this.cerial = cerial;
+    this.serialnumber = serialnumber;
     this.author = author; 
   }
   public String getTitle(){
     return title;
   }
-  public int getCerialnumber(){
-    return cerial;
+  public int getSerialnumber(){
+    return serialnumber;
   }
   public String getAuthor(){
     return author;
   }
+  public void setTitle(String title){
+    this.title = title;
+  }
+  public void setSerialnumber(int serialnumber){
+    this.serialnumber = serialnumber;
+  }
+  public void setAuthor(String author){
+    this.author = author;
+  }
 }
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
