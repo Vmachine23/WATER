eca76f8518ebc035b6e001be00049baa69c6cfaa
Rellshi <202403199@ub.ac.bw>
2025-04-16 16:07:38 +0200

    Update Author.java

--- Author.java
+++ Author.java
@@ -1,25 +1,30 @@
 public class Author{
+
   private String firstName;
   private String lastName;
 
   public Author(String firstName, String lastName){
-    this.firstName=firstName;
-    this.lastName=lastName;
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
-  public String getFirstName(){
+  public String getFirstName() {
     return firstName;
+
   }
 
-  public String getLastName(){
+  public String getLastName() {
     return lastName;
+
   }
 
-  public void setfirstName(String firstName){
+  public void setfirstName(String firstName) {
     this.fistName=firstName;
+
   }
 
-  public void setlastName(String lastName){
+  public void setlastName(String lastName) {
     this.lastName=lastName;
+
   }
 }
   
