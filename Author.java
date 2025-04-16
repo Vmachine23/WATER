72597ac91a038d020760ee8112ccb90e640f4e5c
Angela-2202 <tshegofatsogaolebe@gmail.com>
2025-04-16 15:16:45 +0200

    Create Author.java

---
+++ Author.java
@@ -0,0 +1,25 @@
+public class Author{
+  private String firstName;
+  private String lastName;
+
+  public Author(String firstName, String lastName){
+    this.firstName=firstName;
+    this.lastName=lastName;
+  }
+  public String getFirstName(){
+    return firstName;
+  }
+
+  public String getLastName(){
+    return lastName;
+  }
+
+  public void setfirstName(String firstName){
+    this.fistName=firstName;
+  }
+
+  public void setlastName(String lastName){
+    this.lastName=lastName;
+  }
+}
+  
