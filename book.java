6cf18c5989f52adc5195f4f81e208a4836fd9191
Rellshi <202403199@ub.ac.bw>
2025-04-16 16:19:09 +0200

    Update book.java

--- book.java
+++ book.java
@@ -2,28 +2,45 @@ public class book {
     private String title;
     private int serialNumber;
     private Author author;
+    private String location;
 
-  public book(String title, int serialNumber , String author){
+
+  public book(String title, int serialNumber, String author) {
     this.title = title;
     this.serialNumber = serialNumber;
     this.author = author; 
+    this.location = location;
   }
+
+//Methods
+
+//Getters and Setters
+
   public String getTitle(){
     return title;
   }
-  public int getserialNumber(){
+
+  public int getSerialNumber(){
     return serialNumber;
   }
+
   public String getAuthor(){
     return author;
   }
+
   public void setTitle(String title){
     this.title = title;
   }
-  public void setSerialNumber(int serialNumber){
-    this.serialNumber = serialNumber;
+
+  public void setSerialNumber(int serialNumber){ this.serialNumber = serialNumber;
   }
+
+  public String getLocation() {
+    return location;
+  }
+
   public void setAuthor(String author){
     this.author = author;
   }
+
 }
