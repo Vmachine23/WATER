5b61ebdf0692bc0260983d03086a3f677dfcd852
Angela-2202 <tshegofatsogaolebe@gmail.com>
2025-04-21 18:32:44 +0200

    Update titleSorter.java

--- titleSorter.java
+++ titleSorter.java
@@ -1,4 +1,5 @@
 public class titleSorter{
+  //sorting in ascending order
   public static void selectionsorter(Book[]arr){
     for(int i=0; i < book.size() -1; i++){
       int minIndex=i;
@@ -14,6 +15,33 @@ public class titleSorter{
       Book temp= books.get(i);
       books.set(i,books.get(minIndex));
       books.set(minIndex,temp);
-    }   
+    }
+    System.out.println("\nSorted List in ascending order:");
+        for (Book book : books) {
+            System.out.println(book);
+        }
   }
 }
+
+//sorting in descending order
+public class titleSorter2{
+  public static void selectionsorter(Book[]arr){
+    for(int i = 0; i<book.size()-1; i++{
+      int maxIndex= i;
+      for(int j= i+1; <books.size(); j++{
+        if(books.get(j)getTitle().compareTo(books.get(maxIndex).getTitle())>0){
+          maxIndex=j;
+        }
+      }
+        //swap
+        Book temp= books.get(i);
+        books.set(i, books.get(maxIndex));
+        book.set(maxIndex,temp);
+    }
+      System.out.println("\nSorted List in descending order:");
+      for(Book book : books){
+        System.out.println(book);
+      }
+    }
+}
+
