9d39743421783b34f845317d7e7730863b65e163
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-22 21:13:56 +0100

    Update UtilitySearcher.java
    Added display text for each searcher to show the book details of a located book

--- UtilitySearcher.java
+++ UtilitySearcher.java
@@ -1,27 +1,52 @@
 public class UtilitySearcher{
-    public static int linearSearch(book arr, int target){
-        for (int i=0; i < arr.length; i++){
-            if (arr[i].getSerialNumber() == target) {
-                return i;
+        public static int SearchSerialNumber(Books[] arr, int target){
+            for (int i=0; i < arr.length; i++){
+                if (arr[i].getSerialNumber() == target) {
+                    System.out.println("A book was found with the matching serial number!");
+                    System.out.println("Title: " + arr[i].getTitle());
+                    System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
+                    System.out.println("Serial Number: " + arr[i].getSerialNumber());
+                    System.out.println();
+                    return i;
+                }
             }
+            System.out.println("Sorry... no current book that matches serial number");
+            return -1;
         }
-        return -1;
-    }
-    public static int linearSearch(book arr, int target){
-        for (int i=0; i < arr.length; i++){
-            if (arr[i].getTitle() == target) {
-                return i;
+        public static int SearchTitle(Books[] arr, String target){
+            for (int i=0; i < arr.length; i++){
+                if (arr[i].getTitle().equals(target)) {
+                    System.out.println("Title: " + arr[i].getTitle());
+                    System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
+                    System.out.println("Serial Number: " + arr[i].getSerialNumber());
+                    System.out.println();
+                    return i;
+                }
             }
+            return -1;
         }
-        return -1;
-    }
-     public static int linearSearch(book arr, int target){
-        for (int i=0; i < arr.length; i++){
-            if (arr[i].getAuthor() == target) {
-                return i;
+         public static int SearchFirstName(Books[] arr, String target){
+            for (int i=0; i < arr.length; i++){
+                if (arr[i].getAuthorFirstname().equals(target)) {
+                    System.out.println("Title: " + arr[i].getTitle());
+                    System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
+                    System.out.println("Serial Number: " + arr[i].getSerialNumber());
+                    System.out.println();
+                    return i;
+                }
             }
+            return -1;
         }
-        return -1;
-    }
-        
+        public static int SearchLastName(Books[] arr, String target){
+            for (int i=0; i < arr.length; i++){
+                if (arr[i].getAuthorLastname().equals(target)) {
+                    System.out.println("Title: " + arr[i].getTitle());
+                    System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
+                    System.out.println("Serial Number: " + arr[i].getSerialNumber());
+                    System.out.println();
+                    return i;
+                }
+            }
+            return -1;
+        }
 }
