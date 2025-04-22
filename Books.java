be7be9a87af58ce1d3e8e47c66124dd4660735cf
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-22 18:57:57 +0200

    Book.java
    Same as book.java, except author field has been split to author first name and author last name, making Author class redundant

---
+++ Books.java
@@ -0,0 +1,47 @@
+public class Books {
+    private String title;
+    private int serialNumber;
+    private String authorFirstname;
+    private String authorLastname;
+    
+    public Books(String title, String authorFirstname, String authorLastname, int serialNumber) {
+        this.title = title;
+        this.authorFirstname = authorFirstname;
+        this.authorLastname = authorLastname;
+        this.serialNumber = serialNumber;
+    }
+    //Methods
+    //Getters and Setters
+    public String getTitle(){
+        return title;
+    }
+    
+    public int getSerialNumber(){
+        return serialNumber;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public void setSerialNumber(int serialNumber){ 
+        this.serialNumber = serialNumber;
+    }
+    
+    public String getAuthorFirstname() {
+        return authorFirstname;
+    }
+ 
+    public void setAuthorFirstname(String authorFirstname) {
+        this.authorFirstname = authorFirstname;
+    }
+ 
+    public String getAuthorLastname() {
+        return authorLastname;
+    }
+    
+    public void setAuthorLastname(String authorLastname) {
+        this.authorLastname = authorLastname;
+    }
+}
+
