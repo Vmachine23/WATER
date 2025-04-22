f63ab9ac250d9acaa3d107db6e012baaaf3508a5
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-22 12:58:37 +0200

    Update UtilitySearcher.java
    Changed parameter of first searcher to (book arr, int target) and added searcher for author and title.

--- UtilitySearcher.java
+++ UtilitySearcher.java
@@ -1,10 +1,27 @@
 public class UtilitySearcher{
-    public static int linearSearch(int serialnumber, int target){
+    public static int linearSearch(book arr, int target){
         for (int i=0; i < arr.length; i++){
-            if (arr[i] == target) {
+            if (arr[i].getSerialNumber() == target) {
                 return i;
             }
         }
         return -1;
     }
+    public static int linearSearch(book arr, int target){
+        for (int i=0; i < arr.length; i++){
+            if (arr[i].getTitle() == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+     public static int linearSearch(book arr, int target){
+        for (int i=0; i < arr.length; i++){
+            if (arr[i].getAuthor() == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+        
 }
