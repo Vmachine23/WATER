a92f43b3e1c0499ea4e63b88bc07fd2e46c59cc4
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-22 18:49:10 +0200

    Library.java
    Library class file to add book object to array.

---
+++ Library.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Library{
+    private ArrayList<Books> book = new ArrayList<>();
+
+    public void addBooks(Books b){
+        book.add(b);
+    }
+
+    public void displayBooks(){
+        for (Books b: book){
+            System.out.println("Title: " + b.getTitle());
+            System.out.println("Author: " + b.getAuthorFirstname() + b.getAuthorLastname());
+            System.out.println("Serial Number: " + b.getSerialNumber());
+        }
+    }
+}
