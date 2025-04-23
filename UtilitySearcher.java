d4e6c4d7775ab66449125f22d5d96fa4f422ed95
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-23 16:46:09 +0100

    Update UtilitySearcher.java
    Corrected nesting and strucuring of binarySerialumberSearch method

--- UtilitySearcher.java
+++ UtilitySearcher.java
@@ -1,69 +1,69 @@
 public class UtilitySearcher {
-public static int binarySerialNumberSearch(Books[] arr, int targetScore) {
-        int low = 0;
-        int high = arr.length - 1;
-        while (low <= high){
-             int mid = (low + high) / 2;
-             if (arr[mid].getSerialNumber() == targetScore) {
-                 System.out.println("Title: " + arr[mid].getTitle());
-                 System.out.println("Author: " + arr[mid].getAuthorFirstname() + " " + arr[mid].getAuthorLastname());
-                 System.out.println("Serial Number: " + arr[mid].getSerialNumber());
-                 System.out.println();
-                 return mid;
+    public static int binarySerialNumberSearch(Books[] arr, int targetScore) {
+            int low = 0;
+            int high = arr.length - 1;
+            while (low <= high){
+                 int mid = (low + high) / 2;
+                 if (arr[mid].getSerialNumber() == targetScore) {
+                    System.out.println("Title: " + arr[mid].getTitle());
+                    System.out.println("Author: " + arr[mid].getAuthorFirstname() + " " + arr[mid].getAuthorLastname());
+                    System.out.println("Serial Number: " + arr[mid].getSerialNumber());
+                    System.out.println();
+                    return mid;
                 }
-                else if (arr[mid].getSerialNumber() < targetScore) {
-                    low = mid + 1;
+                    else if (arr[mid].getSerialNumber() < targetScore) {
+                        low = mid + 1;
                     }
                     else {
                         high = mid - 1;
-                        }
-                        }
-                        return -1;
-                        }
-
+                    }
+            }
             System.out.println("Sorry... no current book that matches your serial number...");
             return -1;
-        }
-        public static int SearchTitle(Books[] arr, String target){
-            for (int i=0; i < arr.length; i++){
-                if (arr[i].getTitle().equals(target)) {
-                    System.out.println("A book was found with the matching title!");
-                    System.out.println("Title: " + arr[i].getTitle());
-                    System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
-                    System.out.println("Serial Number: " + arr[i].getSerialNumber());
-                    System.out.println();
-                    return i;
-                }
+    }
+
+    public static int SearchTitle(Books[] arr, String target){
+        for (int i=0; i < arr.length; i++){
+            if (arr[i].getTitle().equals(target)) {
+                System.out.println("A book was found with the matching title!");
+                System.out.println("Title: " + arr[i].getTitle());
+                System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
+                System.out.println("Serial Number: " + arr[i].getSerialNumber());
+                System.out.println();
+                return i;
             }
-            System.out.println("Sorry... no current book that matches your title...");
-            return -1;
         }
-         public static int SearchFirstName(Books[] arr, String target){
-            for (int i=0; i < arr.length; i++){
-                if (arr[i].getAuthorFirstname().equals(target)) {
-                    System.out.println("A book was found with the matching first name!");   
-                    System.out.println("Title: " + arr[i].getTitle());
-                    System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
-                    System.out.println("Serial Number: " + arr[i].getSerialNumber());
-                    System.out.println();
-                    return i;
-                }
+        System.out.println("Sorry... no current book that matches your title...");
+        return -1;
+    }
+    
+    public static int SearchFirstName(Books[] arr, String target){
+        for (int i=0; i < arr.length; i++){
+            if (arr[i].getAuthorFirstname().equals(target)) {
+                System.out.println("A book was found with the matching first name!");   
+                System.out.println("Title: " + arr[i].getTitle());
+                System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
+                System.out.println("Serial Number: " + arr[i].getSerialNumber());
+                System.out.println();
+                return i;
             }
-            System.out.println("Sorry... no current book that matches this author's first name...");
-            return -1;
         }
-        public static int SearchLastName(Books[] arr, String target){
-            for (int i=0; i < arr.length; i++){
-                if (arr[i].getAuthorLastname().equals(target)) {
-                    System.out.println("A book was found with the matching last name!");
-                    System.out.println("Title: " + arr[i].getTitle());
-                    System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
-                    System.out.println("Serial Number: " + arr[i].getSerialNumber());
-                    System.out.println();
-                    return i;
-                }
+        System.out.println("Sorry... no current book that matches this author's first name...");
+        return -1;
+    }
+
+    public static int SearchLastName(Books[] arr, String target){
+        for (int i=0; i < arr.length; i++){
+            if (arr[i].getAuthorLastname().equals(target)) {
+                System.out.println("A book was found with the matching last name!");
+                System.out.println("Title: " + arr[i].getTitle());
+                System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
+                System.out.println("Serial Number: " + arr[i].getSerialNumber());
+                System.out.println();
+                return i;
             }
-            System.out.println("Sorry... no current book that matches this author's last name...");
-            return -1;
         }
+        System.out.println("Sorry... no current book that matches this author's last name...");
+        return -1;
+    }        
 }
