39a16e24a271fa3d74ab3428c53d050f8471c947
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-23 14:48:00 +0200

    Update UtilitySearcher.java
    Added promted messages to relay to user that either book was found or not found given each field.

--- UtilitySearcher.java
+++ UtilitySearcher.java
@@ -10,12 +10,13 @@ public class UtilitySearcher{
                     return i;
                 }
             }
-            System.out.println("Sorry... no current book that matches serial number");
+            System.out.println("Sorry... no current book that matches your serial number...");
             return -1;
         }
         public static int SearchTitle(Books[] arr, String target){
             for (int i=0; i < arr.length; i++){
                 if (arr[i].getTitle().equals(target)) {
+                    System.out.println("A book was found with the matching title!");
                     System.out.println("Title: " + arr[i].getTitle());
                     System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
                     System.out.println("Serial Number: " + arr[i].getSerialNumber());
@@ -23,11 +24,13 @@ public class UtilitySearcher{
                     return i;
                 }
             }
+            System.out.println("Sorry... no current book that matches your title...");
             return -1;
         }
          public static int SearchFirstName(Books[] arr, String target){
             for (int i=0; i < arr.length; i++){
                 if (arr[i].getAuthorFirstname().equals(target)) {
+                    System.out.println("A book was found with the matching first name!");   
                     System.out.println("Title: " + arr[i].getTitle());
                     System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
                     System.out.println("Serial Number: " + arr[i].getSerialNumber());
@@ -35,11 +38,13 @@ public class UtilitySearcher{
                     return i;
                 }
             }
+            System.out.println("Sorry... no current book that matches this author's first name...");
             return -1;
         }
         public static int SearchLastName(Books[] arr, String target){
             for (int i=0; i < arr.length; i++){
                 if (arr[i].getAuthorLastname().equals(target)) {
+                    System.out.println("A book was found with the matching last name!");
                     System.out.println("Title: " + arr[i].getTitle());
                     System.out.println("Author: " + arr[i].getAuthorFirstname() + " " + arr[i].getAuthorLastname());
                     System.out.println("Serial Number: " + arr[i].getSerialNumber());
@@ -47,6 +52,7 @@ public class UtilitySearcher{
                     return i;
                 }
             }
+            System.out.println("Sorry... no current book that matches this author's last name...");
             return -1;
         }
 }
