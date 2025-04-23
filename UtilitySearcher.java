9408440ffb2ab915de3c5efb9ab1b08d90a6c3e6
SwitchWay <162799741+aabc123@users.noreply.github.com>
2025-04-23 16:44:53 +0200

    Update UtilitySearcher.java
    update

--- UtilitySearcher.java
+++ UtilitySearcher.java
@@ -1,19 +1,25 @@
-public class UtilitySearcher{
-        public static int binarySerialNumberSearch(Books[] arr, int targetScore) {
-    int low = 0;
-    int high = arr.length - 1;
-     while (low <= high) {
-      int mid = (low + high) / 2;
- if (arr[mid].getSerialNumber() == targetScore) {
- return mid;
- } else if (arr[mid].getSerialNumber() < targetScore) {
- low = mid + 1;
- } else {
- high = mid - 1;
- }
- }
- return -1;
-}
+public class UtilitySearcher {
+public static int binarySerialNumberSearch(Books[] arr, int targetScore) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high){
+             int mid = (low + high) / 2;
+             if (arr[mid].getSerialNumber() == targetScore) {
+                 System.out.println("Title: " + arr[mid].getTitle());
+                 System.out.println("Author: " + arr[mid].getAuthorFirstname() + " " + arr[mid].getAuthorLastname());
+                 System.out.println("Serial Number: " + arr[mid].getSerialNumber());
+                 System.out.println();
+                 return mid;
+                }
+                else if (arr[mid].getSerialNumber() < targetScore) {
+                    low = mid + 1;
+                    }
+                    else {
+                        high = mid - 1;
+                        }
+                        }
+                        return -1;
+                        }
 
             System.out.println("Sorry... no current book that matches your serial number...");
             return -1;
