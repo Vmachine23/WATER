ac0a4618b8fc6ffada2f35d0a30cc2035e9de940
SkinnyLegend123 <rethabilethabo3@gmail.com>
2025-04-27 09:55:02 +0100

    Update InvalidInputException.java
    Removed unnecessary main class and customised readAndparse method to display appropriate message for serial number searcher

--- InvalidInputException.java
+++ InvalidInputException.java
@@ -1,31 +1,21 @@
 import java.util.Scanner;
 
-public class InvalidInputException {
+public class InvalidInputException extends Exception{
     public InvalidInputException(String message) {
         super(message);
     }
     public static int readAndParse() throws InvalidInputException {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
+        System.out.print("What is the target serial number you are looking for: ");
         String input = sc.nextLine();
         try {
         return Integer.parseInt(input);
         } 
         catch (NumberFormatException e) {
-        throw new InvalidInputException("Not a valid integer: " + input);
+        throw new InvalidInputException("Invalid. The serial number is not an integer!: " + input);
         } 
         finally {
         System.out.println("Invalid exception caught!");
-        }
-    }
-    
-    public static void main(String[] args) {
-        try {
-        int value = readAndParse();
-        System.out.println("Parsed int: " + value);
-        } 
-        catch (InvalidInputException e) {
-        System.out.println("Error: " + e.getMessage());
         }
     }
 }
