ce40e6d145085ea1bcbf07252f95804e2503d212
SwitchWay <162799741+aabc123@users.noreply.github.com>
2025-04-30 18:21:31 +0200

    Update InvalidInputException.java
    Adding comments

--- InvalidInputException.java
+++ InvalidInputException.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//Custom made ezception to catch invalid entered integer during input stage
 public class InvalidInputException extends Exception{
     public InvalidInputException(String message) {
         super(message);
@@ -9,10 +9,10 @@ public class InvalidInputException extends Exception{
         System.out.print("What is the target serial number you are looking for: ");
         String input = sc.nextLine();
         try {
-        return Integer.parseInt(input);
+        return Integer.parseInt(input); //Checks the integer put
         } 
         catch (NumberFormatException e) {
-        throw new InvalidInputException("Invalid. The serial number is not an integer!: " + input);
+        throw new InvalidInputException("Invalid. The serial number is not an integer!: " + input); //Catches the invalid integer put
         } 
         finally {
         System.out.println("Invalid exception caught!");
