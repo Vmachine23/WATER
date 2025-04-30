9fbfda7cffbb68be2e44c818c775961b88653b64
SwitchWay <162799741+aabc123@users.noreply.github.com>
2025-04-30 18:23:40 +0200

    Update Library.java
    Adding comments

--- Library.java
+++ Library.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
-
+//Custom made array list
 public class Library{
     private ArrayList<Books> book = new ArrayList<>();
-
+//Method to add books in the library
     public void addBooks(Books b){
         book.add(b);
     }
-
+//Output for books in the library
     public void displayBooks(){
         for (Books b: book){
             System.out.println("Title: " + b.getTitle());
